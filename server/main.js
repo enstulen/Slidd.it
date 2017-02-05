d4960e1802ff0654e3ea03fa092522d83a7ca643
Morten Stulen <enstulen@gmail.com>
2017-02-05 15:59:11 +0100

    Create user class and update value for each user

--- server/main.js
+++ server/main.js
@@ -1,6 +1,19 @@
-import { Meteor } from 'meteor/meteor';
 import '../imports/api/slidervalues.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+// Meteor.methods({
+//   removeAllUsers:function(){
+//     if (Meteor.isServer) {
+//     Meteor.startup(function() {
+//       return Meteor.methods({
+//         removeAllPosts: function() {
+//           return SliderValues.remove({});
+//         }
+//       });
+//     });
+//   }
+//   }
+// });
