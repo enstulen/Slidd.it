19220406d808d081db92e96c570eb71b9b7ae862
annettejg <annette.giortz@gmail.com>
2017-02-27 13:00:08 +0100

    Added navbar

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { mount } from 'react-mounter';
@@ -7,31 +7,35 @@ import { mount } from 'react-mounter';
 import { SliderWrapper } from './SliderWrapper';
 import { CurrentUser } from '../startup/xUser';
 import { GaugeWrapper } from './GaugeWrapper';
+import { NavbarHeader } from './NavbarHeader'
 
 import { createContainer } from 'meteor/react-meteor-data';
 import { SliderValues } from '../api/sliderValues/slidervalues.js';
 
-class App extends React.Component{
-  render(){
-    return(
-      <div id="centerBox">
-        <div>
-          <h1>😊 Slidd.it 😊</h1>
-        </div>
-        <div className="gauge">
-          <GaugeWrapper sliderValues={this.props.sliderValues}/>
-        </div>
-        <div className="slider">
-          <SliderWrapper />
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div id="centerBox">
+          <div className="navbar">
+            <NavbarHeader />
+          </div>
+          <div className="gauge">
+            <GaugeWrapper sliderValues={this.props.sliderValues} />
+          </div>
+          <div className="slider">
+            <SliderWrapper />
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
+
 
 export default createContainer(() => {
   Meteor.subscribe('sliderValues.all');
   return {
-      sliderValues: SliderValues.find({}).fetch(),
+    sliderValues: SliderValues.find({}).fetch(),
   };
 }, App);
