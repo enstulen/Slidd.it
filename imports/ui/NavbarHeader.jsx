495807f2a03604e5b528c6f8bd823a8d32c06cfa
annettejg <annette.giortz@gmail.com>
2017-02-28 15:28:23 +0100

    Lint errors

--- imports/ui/NavbarHeader.jsx
+++ imports/ui/NavbarHeader.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Welcome from '../ui/pages/welcome.jsx';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
-export class NavbarHeader extends Component {
-
+export default class NavbarHeader extends Component {
 
   handleButtonPress(event) {
-    FlowRouter.go('/')
+    this.event = event;
+    event.preventDefault();
+    FlowRouter.go('/');
   }
 
   render() {
@@ -21,19 +21,19 @@ export class NavbarHeader extends Component {
                 <span className="icon-bar" />
                 <span className="icon-bar" />
               </button>
-              <a className="navbar-brand" href="#">
+              <a className="navbar-brand">
                 <img className="img-responsive" alt="Slidd.it" src="logo3.png" />
               </a>
             </div>
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
-                <li><a href="#" onClick={this.handleButtonPress.bind(this)}>Home</a></li>
-                  <li className="dropdown">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Active lectures <span className="caret"></span></a>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-                      <li><a href="#">Action</a></li>
-                    </ul>
-                  </li>
+                <li><a href="hei" onClick={this.handleButtonPress}>Home</a></li>
+                <li className="dropdown">
+                  <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Active lectures <span className="caret" /></a>
+                  <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a>Action</a></li>
+                  </ul>
+                </li>
               </ul>
             </div>
           </div>
