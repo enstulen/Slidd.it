318efe1e9d6e5d5900c01b02efd4d9d3fa58f769
Morten Stulen <enstulen@gmail.com>
2017-03-14 18:07:43 +0100

    Change cronjob to 24 hours

--- imports/startup/server/cron.js
+++ imports/startup/server/cron.js
@@ -4,12 +4,12 @@ import { SliderValues } from '../../api/sliderValues/slidervalues';
 SyncedCron.add({
   name: 'Remove old users',
   schedule(parser) {
-    return parser.text('every 1 min');
+    return parser.text('every 24 hours');
   },
   job() {
     const today = new Date();
     const MS_PER_MINUTE = 60000;
-    const targetDate = new Date(today - (1 * MS_PER_MINUTE));
+    const targetDate = new Date(today - (60 * 24 * MS_PER_MINUTE));
     SliderValues.remove({ createdAt: { $lt: targetDate } });
   },
 });
