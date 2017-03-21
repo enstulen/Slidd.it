be97b9d5723f527262ac22d50298209ec04b060d
halvorbmundal <halvorbmundal@gmail.com>
2017-03-21 19:31:04 +0100

    Fix a bug where the logo image would disappear, also aling the logo
    The logo image is still a few pixels off

--- imports/ui/NavbarHeader.jsx
+++ imports/ui/NavbarHeader.jsx
@@ -1,15 +1,11 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { createContainer } from 'meteor/react-meteor-data';
-
-import { Lectures } from '../api/lectures/lectures.js';
 
 import Lecture from './Lecture.jsx';
 
 export default class NavbarHeader extends Component {
 
   handleButtonPress(event) {
-    this.event = event;
     event.preventDefault();
     FlowRouter.go('/');
   }
@@ -31,13 +27,13 @@ export default class NavbarHeader extends Component {
                 <span className="icon-bar" />
                 <span className="icon-bar" />
               </button>
-              <a className="navbar-brand">
-                <img className="img-responsive" onClick={this.handleButtonPress} width="105" alt="Slidd.it" src="logo3.png" />
-              </a>
+              <button className="invisibleButton" onClick={this.handleButtonPress}>
+                <img className="img-responsive" width="180" alt="Slidd.it" src="/logo3.png" />
+              </button>
             </div>
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
-                <li><a href="/" onClick={this.handleButtonPress}>Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li className="dropdown">
                   <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Active lectures <span className="caret" /></a>
                   <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
