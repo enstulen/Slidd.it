a016caa7c9969021a17203bca592aa9d39999d37
Konstantin <konstantin@live.no>
2017-03-29 20:36:18 +0200

    Fix unit test slidervalues so it actually tests units

--- imports/api/sliderValues/slidervalues.test.js
+++ imports/api/sliderValues/slidervalues.test.js
@@ -7,23 +7,37 @@ import { SliderValues } from './slidervalues.js';
 if (Meteor.isServer) {
   describe('SliderValues', () => {
     describe('Methods', () => {
+      // Empties database
       resetDatabase();
       SliderValues.remove({});
-      // The database should be empty while we do this right?
-      const count = SliderValues.find().count();
 
-      it('Can insert slidervalue', () => {
-        // Inserts an user, currently values are not set for unknown reason.
-        SliderValues.insert({ _id: 'user2' }, { $set: { value: 52, lectureName: 'lectureName', createdAt: new Date() } });
+      // Sets values to be tested
+      const count = SliderValues.find().count();
+      const userName = 'user2';
+      const lectureName = 'lectureName';
+
+      it('Can insert sliderValue', () => {
+        Meteor.call('sliderValues.insert', userName, 52, lectureName, new Date());
+        // Returns a collection
+        const result = SliderValues.find().fetch()[0];
+
+        // tests if the database contains 1 element with the same values we sent in
         assert.equal(SliderValues.find().count(), (count + 1));
+        assert.equal(result.userID, userName);
+        assert.equal(result.value, 52);
+        assert.equal(result.lectureName, lectureName);
       });
 
-      it('Can update slidervalue', () => {
-        SliderValues.update({ _id: 'user2' }, { $set: { value: 47 } });
-        // Returns all users in database, currently only one. Returns as an array,
-        // so we choose the first and only one
+      it('Can update sliderValue', () => {
+        Meteor.call('sliderValues.update', userName, 47);
+
         const result = SliderValues.find().fetch()[0];
+
+        // Tests that only the value we updated got updated
+        assert.equal(result.userID, userName);
         assert.equal(result.value, 47);
+        assert.equal(SliderValues.find().count(), (count + 1));
+        assert.equal(result.lectureName, 'lectureName');
       });
     });
   });
